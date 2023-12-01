86cfa49ec4412508cabfc3ccdc398031907798cf
Ian Guedes <114230677+iangpm@users.noreply.github.com>
2023-12-01 11:27:07 -0300

    Add files via upload

---
+++ #Nisston.py
@@ -0,0 +1,21 @@
+#Nisston
+# Exercício 1
+#  A) 10 nós     B) 3          C) H         D) A, C, J, L, M e N      E) B, F e E
+#  F) A, C, J e E        G) H e F         H) J, E, L, M e N
+
+# Exercício 2
+# A) D -> B -> A -> C -> F -> E -> G
+# B) A -> B -> C -> D -> E -> F -> G
+# C) A -> B -> C -> E -> F -> G -> D
+
+# Exercício 3
+# A) Bubble: 7.5817   Select: 0.0001      Quick: 9.0122
+# B) Bubble: 8.5115   Select: 9.0122      Quick: 0.0001
+# C) Bubble: 8.4638   Select: 9.9658      Quick: 0.0001
+
+# Exercício 4
+# A) Pré-ordem: 60    Em ordem: 23       Pós-ordem: 32
+
+# Exercício 5
+# A)      B) 20 e 10     C) 60 e 50    D) Sim             E) 59
+# F) 55   G) 15 
